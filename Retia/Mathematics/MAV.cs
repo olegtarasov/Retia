e07bc0ef9469a60d9596edf70e6f203c5265e0e7
Roman Aristov <r.aristov@gmail.com>
2017-02-17 17:17:04 +0300

    MAV fixed

--- Retia/Mathematics/MAV.cs
+++ Retia/Mathematics/MAV.cs
@@ -1,53 +1,29 @@
-﻿namespace Retia.Mathematics
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Retia.Mathematics
 {
     public class MAV
     {
         private readonly int _order;
-
-        private double _last = 0.0d;
-        private double _average = 0.0d;
-        private int _ticks = 0;
-
+        private readonly List<double> _buffer; 
         public MAV(int order)
         {
             _order = order;
+            _buffer=new List<double>(_order);
         }
 
         public double Filter(double value)
         {
-            if (_ticks < _order)
-            {
-                _ticks++;
-            }
-
-            if (_ticks == 1)
-            {
-                _average = value;
-            }
-            else
-            {
-                int order = _ticks < _order ? _ticks : _order;
-                double cur = value / order;
-
-                if (_ticks < order)
-                {
-                    _average += cur;
-                }
-                else
-                {
-                    _average = _average - _last + cur;
-                }
-                
-                _last = cur;
-            }
-
-            return _average;
+            _buffer.Add(value);
+            if(_buffer.Count>_order)
+                _buffer.RemoveAt(0);
+            return _buffer.Sum()/_buffer.Count;
         }
 
         public void Reset()
         {
-            _average = _last = 0.0d;
-            _ticks = 0;
+            _buffer.Clear();
         }
     }
 }
