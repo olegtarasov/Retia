fd08ea774cd8cc7067df756c4a4764ed0fd5960a
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-02-18 18:22:17 +0300

    Added tests to match C# algorithms to Thrust versions

--- Retia.Cuda/Algorithms.h
+++ Retia.Cuda/Algorithms.h
@@ -30,14 +30,6 @@ struct ErrorPropagationFunctor : public thrust::binary_function<float, float, fl
 
 struct CrossEntropyErrorFunctor
 {
-	/*const int _columns;
-
-
-	explicit CrossEntropyErrorFunctor(int columns)
-		: _columns(columns)
-	{
-	}*/
-
 	template <typename Tuple>
 	__host__ __device__
 	float operator()(Tuple t)
