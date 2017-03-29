a0467036d8796ea1a72f7a12685299d33b617a63
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-03-29 10:35:15 +0300

    Starting to migrate CI to VS 2017

--- init_ci.cmd
+++ init_ci.cmd
@@ -1,19 +1,23 @@
 @echo off
 echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
+rem appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
+appveyor DownloadFile  https://www.dropbox.com/s/ll2ay531hw3i1p7/cuda.zip?dl=1
+7z x cuda.zip -ocuda
+cd cuda
 echo Installing CUDA toolkit 8
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
+setup.exe -s compiler_8.0 ^
                            cublas_8.0 ^
                            cublas_dev_8.0 ^
                            cudart_8.0 ^
                            curand_8.0 ^
-                           curand_dev_8.0 ^
-                           visual_studio_integration_8.0
+                           curand_dev_8.0 
 
 if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin\cudart64_80.dll" ( 
 echo "Failed to install CUDA"
 exit /B 1
 )
+
+cd ..
                            
 echo Downloading cuDNN
 appveyor DownloadFile https://www.dropbox.com/s/9t56hoewk7p0hfj/cudnn-8.0-windows7-x64-v5.1.zip?dl=1 -FileName cudnn-8.0-windows7-x64-v5.1-zip
@@ -31,13 +35,13 @@ set CUDA_PATH_V8_0=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0
 
 nvcc -V
 
-cd ..
-git clone https://github.com/Microsoft/vcpkg.git vcpkg
-cd vcpkg
-mkdir downloads
-copy nul downloads\AlwaysAllowDownloads
-powershell -exec bypass scripts\bootstrap.ps1
-vcpkg integrate install
-vcpkg install gtest:x64-windows
+rem cd ..
+rem git clone https://github.com/Microsoft/vcpkg.git vcpkg
+rem cd vcpkg
+rem mkdir downloads
+rem copy nul downloads\AlwaysAllowDownloads
+rem powershell -exec bypass scripts\bootstrap.ps1
+rem vcpkg integrate install
+rem vcpkg install gtest:x64-windows
 
 cd "%APPVEYOR_BUILD_FOLDER%"
