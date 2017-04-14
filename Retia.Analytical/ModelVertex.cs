d93be9bb6146e1340fe8dac5d0697a0c5eb67343
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-04-14 17:01:01 +0300

    Started analytical engine

---
+++ Retia.Analytical/ModelVertex.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Retia.Analytical
+{
+    public class ModelVertex
+    {
+        public ModelVertex(string name, Expression expression)
+        {
+            Expression = expression;
+            Name = name;
+        }
+
+        public string Name { get; }
+        public Expression Expression { get; }
+    }
+}
